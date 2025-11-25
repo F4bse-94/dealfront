c29c2b244776651425013f5634eb12af69c9d4a8
F4bse-94 <fabian.smog@googlemail.com>
2025-11-25 18:52:44 +0100

    Add MCP server spawning functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import { spawn } from "child_process";
+
+const proc = spawn("npx", [
+  "apidog-mcp-server@latest",
+  "--site-id=359998"
+]);
+
+proc.stdout.on("data", (data) => process.stdout.write(data));
+proc.stderr.on("data", (data) => process.stderr.write(data));
+proc.on("close", (code) => {
+  console.log("MCP server exited with code", code);
+});
 
